9b5e7bb4bd1820b389f26852799a0d3eb3bc3de2
mukesh rawat <mukeshrawat02@gmail.com>
2019-01-28 23:46:24 +0000

    Bootstrap support added in the project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
