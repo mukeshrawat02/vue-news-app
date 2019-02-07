e41508789231456ad1266c80609fc11d006d67a6
mukesh rawat <mukeshrawat02@gmail.com>
2019-02-07 11:48:37 +0000

    Updated News Service with correct return type.

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -5,11 +5,11 @@ import config from "../config";
 
 class NewsService {
 
-  public async getTopicHeadlines(topic: string): Promise<NewsDto> {
+  public async getTopicHeadlines(topic: string): Promise<any> {
     const key = config.newsApiKey;
     const url = `${config.newsApiUrl}?q=${topic}&apiKey=${key}`;
 
-    return await axios.get<NewsDto>(url);
+    return await axios.get(url);
   }
 }
 
