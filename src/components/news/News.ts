8b170e93d878ab9800e6a2174b35a52aef5edb64
mukesh rawat <mukeshrawat02@gmail.com>
2019-02-07 11:48:06 +0000

    Intergrate News-Item compnent in News and render news card as per the api response.

--- src/components/news/News.ts
+++ src/components/news/News.ts
@@ -1,5 +1,8 @@
-import { Component, Vue, Prop } from "vue-property-decorator";
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+
 import NewsItem from "../news-item/News-Item";
+import { newsService } from "../../services/news.service";
+import { NewsDto, Article } from "../../models/news";
 
 @Component({
   components: {
@@ -7,9 +10,22 @@ import NewsItem from "../news-item/News-Item";
   }
 })
 export default class News extends Vue {
-  @Prop() public topic: string | undefined;
+  @Prop() public topic: string;
+  selectedNews: Article | null = null;
+  articles: Article[] = [];
 
-  constructor() {
-    super();
+  @Watch("topic") onRouteChange(currentValue: string, oldValue: string) {
+    if (currentValue !== oldValue) {
+      this.getNews();
+    }
+  }
+
+  async getNews() {
+    await newsService
+      .getTopicHeadlines(this.topic)
+      .then((response: any) => {
+        this.articles = response.data.articles;
+      })
+      .catch(err => console.log(err));
   }
 }
